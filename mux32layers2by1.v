43994e4993ccc581624db83061d08af5cea30b41
PhilipZLoh <philip.loh@students.olin.edu>
2012-10-29 16:51:44 -0400

    worked on Shifter, added stub mux32layers2by1

--- mux32layers2by1.v
+++ mux32layers2by1.v
@@ -1,11 +1,5 @@
-/* mux2by1.v
+/* mux32layers2by1.v
 part of project vALU by FluffyTheGatekeeper
 Julian Ceipek, Yuxin Guan, Philip Z Loh, Sasha Sproch
 Computer Architecture, Olin College Fall 2012 */
 
-module mux2by1(out, s, ins);
-  output out;
-  input s;
-  input [1:0] ins;
-  assign out = ins[s];
-endmodule
