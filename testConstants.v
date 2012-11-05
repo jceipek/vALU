caedbaa901de9c9a0b7a0a9f6dc87e99e533dfca
Julian Ceipek <julian.ceipek@gmail.com>
2012-11-04 21:11:34 -0500

    Added a define for XOR.

---
+++ testConstants.v
@@ -0,0 +1,10 @@
+/* testConstants.v
+part of project vALU by FluffyTheGatekeeper
+Julian Ceipek, Yuxin Guan, Philip Z Loh, Sasha Sproch
+Computer Architecture, Olin College Fall 2012 */
+
+`define FAIL isFail = 1; $display("FAIL! time %d", $time)
+`define CLK #10
+`define DELAY #50
+
+`define XOR #20
