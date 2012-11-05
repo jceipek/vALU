10f0bc0d6e7c6353acc6132a3f1b7dec6b24ea69
SashaSproch <sashasproch@gmail.com>
2012-11-05 16:50:48 -0500

    playing not much really

--- testConstants.v
+++ testConstants.v
@@ -4,5 +4,5 @@ Julian Ceipek, Yuxin Guan, Philip Z Loh, Sasha Sproch
 Computer Architecture, Olin College Fall 2012 */
 
 `define FAIL $display("FAIL! time %d", $time)
-`define CLK #1500
+`define CLK #1700
 `define DELAY #50
