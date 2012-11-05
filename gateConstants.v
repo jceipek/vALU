c56e38e6cecd4d4e9fa00cf85b26ed80584853a8
Julian Ceipek <julian.ceipek@gmail.com>
2012-11-04 21:43:23 -0500

    Fixed syntax errors

--- gateConstants.v
+++ gateConstants.v
@@ -3,7 +3,7 @@ part of project vALU by FluffyTheGatekeeper
 Julian Ceipek, Yuxin Guan, Philip Z Loh, Sasha Sproch
 Computer Architecture, Olin College Fall 2012 */
 
-`define XOR #20
+`define XOR xor #20
 `define AND #20
 `define OR #20
 `define MUX2BY1 #80
--- testConstants.v
+++ testConstants.v
@@ -3,6 +3,6 @@ part of project vALU by FluffyTheGatekeeper
 Julian Ceipek, Yuxin Guan, Philip Z Loh, Sasha Sproch
 Computer Architecture, Olin College Fall 2012 */
 
-`define FAIL isFail = 1; $display("FAIL! time %d", $time)
+`define FAIL $display("FAIL! time %d", $time)
 `define CLK #10
 `define DELAY #50
