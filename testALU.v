3ce375beface1d10f1fbc465fc8ae1a32de9c5a7
SashaSproch <sashasproch@gmail.com>
2012-11-05 06:35:36 -0500

    Added testALU am now happy.

---
+++ testALU.v
@@ -0,0 +1,35 @@
+/* ALU.v
+part of project vALU by FluffyTheGatekeeper
+Julian Ceipek, Yuxin Guan, Philip Z Loh, Sasha Sproch
+Computer Architecture, Olin College Fall 2012 */
+
+`include "ALU.v"
+`include "testConstants.v"
+
+module testALU();
+
+ALU myALU(out, Zero, Overflow, Cout, S, A, B);
+  wire [31:0] out;
+  wire Zero, Overflow, Cout;
+  
+  reg [2:0] S;
+  reg [31:0] A, B;
+  
+  initial begin
+    
+    A = 000000000;
+    B = 0000000000;
+    S = 000;
+    if (out !== 000) begin
+      `FAIL;
+    end
+    
+    
+    
+    
+    
+  end
+  
+  
+endmodule
+  
